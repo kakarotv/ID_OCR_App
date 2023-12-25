d71e6c573a6992396bea2eb6ddb14e6fb3f726fb
kakarotv <vivsn3@gmail.com>
2023-12-25 19:38:53 +0530

    display all entries

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const multer = require('multer');
 const tesseract = require('node-tesseract-ocr');
 const path = require('path');
-const fs = require('fs/promises'); 
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/uploads')));
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb+srv://vivsn3:jzLWWIz6Wjj1t3wU@cluster0.jc2uj7q.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -17,8 +27,30 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+const ocrSchema = new mongoose.Schema({
+  idNumber: String,
+  name: String,
+  lastName: String,
+  dateOfBirth: String,
+  dateOfIssue: String,
+  dateOfExpiry: String,
+});
+
+const OCRModel = mongoose.model('OCRModel', ocrSchema);
+
+
 app.get('/', (req, res) => {
-  res.render('index', { extractedInfo: null, outputJSON: '' }); // Render the index view initially with empty extracted information and JSON
+  res.render('index', { extractedInfo: null, outputJSON: '' });
+});
+
+app.get('/printAllOCRData', async (req, res) => {
+  try {
+    const allRecords = await OCRModel.find({});
+    res.json(allRecords);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Error retrieving all OCR records' });
+  }
 });
 
 app.post('/extracttextfromimage', upload.single('file'), async (req, res) => {
@@ -29,26 +61,20 @@ app.post('/extracttextfromimage', upload.single('file'), async (req, res) => {
   };
 
   try {
-    const text = await tesseract.recognize(req.file.path, config); // Perform OCR on the uploaded image
+    const text = await tesseract.recognize(req.file.path, config);
 
-    // Extract specific information using regular expressions
     const extractedInfo = extractInformationFromOCR(text);
 
-    // Convert the extracted info to JSON
-    const outputJSON = JSON.stringify(extractedInfo, null, 2);
+    const ocrData = new OCRModel(extractedInfo);
+    await ocrData.save();
 
-    // Write the JSON data to a file named output.json
-    await fs.writeFile('uploads/output.json', outputJSON);
-
-    // Render the extracted information and output JSON on the UI
-    res.render('index', { extractedInfo, outputJSON });
+    res.render('index', { extractedInfo, outputJSON: JSON.stringify(extractedInfo, null, 2) });
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).send('Error extracting text');
   }
 });
 
-// Function to extract information from the OCR text using regular expressions
 function extractInformationFromOCR(text) {
   const idNumberRegex = /identification Number (\d{1,} \d{1,} \d{1,} \d{1,} \d{1,})/i;
   const nameRegex = /Name ([\w\s]+)(?=\s*Lastname)/i;
@@ -56,9 +82,7 @@ function extractInformationFromOCR(text) {
   const dobRegex = /Date of Birth (\d{1,2} [A-Za-z]+\. \d{4})/i;
   const issueDateRegex = /Date of Issue (\d{1,2} [A-Za-z]+\. \d{4})/i;
   const expiryDateRegex = /Date of Expiry (\d{1,2} [A-Za-z]+\. \d{4})/i;
-  
 
-  // Match regex patterns in the OCR text
   const idNumberMatch = text.match(idNumberRegex);
   const nameMatch = text.match(nameRegex);
   const lastNameMatch = text.match(lastNameRegex);
@@ -66,7 +90,6 @@ function extractInformationFromOCR(text) {
   const issueDateMatch = text.match(issueDateRegex);
   const expiryDateMatch = text.match(expiryDateRegex);
 
-  // Construct extracted information object
   const extractedInfo = {
     idNumber: idNumberMatch ? idNumberMatch[1] : null,
     name: nameMatch ? nameMatch[1] : null,
@@ -78,6 +101,17 @@ function extractInformationFromOCR(text) {
 
   return extractedInfo;
 }
+
+// Error handling for unhandled routes
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({ error: 'Something went wrong' });
+});
 
 app.listen(5000, () => {
   console.log('App is listening on port 5000');
